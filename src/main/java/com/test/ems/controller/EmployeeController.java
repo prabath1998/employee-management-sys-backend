f9b8e6ba4d1e7ff313508a1fd88d5b4295a47996
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-30 12:32:03 +0530

    add deleteEmployee rest api

--- src/main/java/com/test/ems/controller/EmployeeController.java
+++ src/main/java/com/test/ems/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.test.ems.controller;
 import com.test.ems.model.Employee;
 import com.test.ems.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -22,5 +25,14 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted",deleted);
+        return ResponseEntity.ok(response);
     }
 }
